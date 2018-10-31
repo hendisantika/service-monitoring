063eb14c4d767094727bbd1f620d212b8ddd25f2
Hendi Santika <hendisantika@gmail.com>
2018-11-01 06:50:37 +0700

    #2 - Add person-application

---
+++ person-application/src/main/java/com/hendisantika/personapplication/PersonApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.personapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApplication.class, args);
+    }
+}
