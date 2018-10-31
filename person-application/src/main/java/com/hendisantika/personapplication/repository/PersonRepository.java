7050657a1c3918ddcbe75c8188ce47b56f3ce30d
Hendi Santika <hendisantika@gmail.com>
2018-11-01 06:56:31 +0700

    #6 - Add Person Repository - person-application

---
+++ person-application/src/main/java/com/hendisantika/personapplication/repository/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.hendisantika.personapplication.repository;
+
+import com.hendisantika.personapplication.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-application
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/18
+ * Time: 06.56
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+}
