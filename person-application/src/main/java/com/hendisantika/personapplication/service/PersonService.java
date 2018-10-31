77edeff735a48aef636c7c8843b1f9ee2104ec21
Hendi Santika <hendisantika@gmail.com>
2018-11-01 06:58:31 +0700

    #7 - Add Person Service - person-application

---
+++ person-application/src/main/java/com/hendisantika/personapplication/service/PersonService.java
@@ -0,0 +1,47 @@
+package com.hendisantika.personapplication.service;
+
+import com.hendisantika.personapplication.entity.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-application
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/18
+ * Time: 06.56
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+public interface PersonService {
+    /**
+     * @param personId
+     * @return {@link Optional} {@link Person} objects if present in database
+     * for supplied person ID
+     */
+    Optional<Person> getPersonById(int personId);
+
+    /**
+     * @return {@link List} of {@link Person} model class fo rall available
+     * entities
+     */
+    List<Person> getAllPersons();
+
+    /**
+     * @param personId
+     * @return Delete the person from database for supplied id
+     */
+    boolean removePerson(int personId);
+
+    /**
+     * @param person
+     * @return {@link Optional} {@link Person} objects after save or update Save
+     * if no personId present else update
+     */
+    Optional<Person> saveUpdatePerson(Person person);
+}
