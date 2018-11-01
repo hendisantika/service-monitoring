a873252d39b4312a5bbb0a1dc2ddbde44d337fc5
Hendi Santika <hendisantika@gmail.com>
2018-11-01 07:00:29 +0700

    #9 - Add Person Model - person-application

---
+++ person-application/src/main/java/com/hendisantika/personapplication/Person.java
@@ -0,0 +1,30 @@
+package com.hendisantika.personapplication;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-application
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/18
+ * Time: 07.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person implements Serializable {
+    private static final long serialVersionUID = 3570556679061270028L;
+
+    private int personId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
