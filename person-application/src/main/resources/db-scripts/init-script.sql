f724c41fa168208b93ff813804f7921c19df9a6e
Hendi Santika <hendisantika@gmail.com>
2018-11-01 07:21:19 +0700

    #14 - Add init script - person-application

---
+++ person-application/src/main/resources/db-scripts/init-script.sql
@@ -0,0 +1,4 @@
+insert into PERSON (PERSON_ID,FIRST_NAME,LAST_NAME,EMAIL) values
+(1, 'Uzumaki','Naruto','naruto@konohagakure.com'),
+(2, 'Uchiha','Sasuke','sasuke@konohagakure.com'),
+(3, 'Sasuke','Haruno','sakura@konohagakure.com');
